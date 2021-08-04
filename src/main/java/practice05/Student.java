81d9712d25037098f48b77fa9fad6bbac2715c27
kudoyuto <yuto.kudo@oocl.com>
2021-08-04 17:55:47 +0800

    [OOPPractice][Yuto Kudo] feat: Implementation of Practice 05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,21 @@
 package practice05;
 
-public class Student {
+import java.text.MessageFormat;
+
+public class Student extends Person{
+    private int klass;
+    public Student(String name, int age, int klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+    @Override
+    public String introduce() {
+        return super.introduce() + MessageFormat.format((" I am a Student. I am at Class {0}."),klass);
+
+    }
+
+
+    public int getKlass() {
+        return klass;
+    }
 }
--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,27 @@
 package practice05;
 
-public class Teacher {
+import java.text.MessageFormat;
+
+public class Teacher extends Person {
+    private Integer klass;
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public Teacher(String name, int age, int klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(this.klass!=null) {
+            return super.introduce() + MessageFormat.format((" I am a Teacher. I teach Class {2}."),getName(),getAge(),klass);
+        }else {
+            return super.introduce() + MessageFormat.format((" I am a Teacher. I teach No Class."),getName(),getAge());
+        }
+    }
 }
