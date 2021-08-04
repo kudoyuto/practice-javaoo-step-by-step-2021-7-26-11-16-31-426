0681629a38f1dc184ff814cdf5a0dca64d480ad9
kudoyuto <yuto.kudo@oocl.com>
2021-08-05 00:21:39 +0800

    [OOPPractice][Yuto Kudo] feat: Implementation of Practice 07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,32 @@
 package practice07;
 
+import java.util.List;
+
+//
+//        The `klass` attribute is an `Object` which has `number` and `leader` attribute.
+//        But the `leader` attribute is `not` in the constructor.
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+
+    public Klass(int number) {
+        this.number=number;
+    }
+    public Integer getNumber(){
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+ this.number;
+    }
+
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Student assignLeader(Student student) {
+        return this.leader = student;
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,28 @@
 package practice07;
+//Write another `Student` class which inherit from the `Person` class.
+//        Besides `id`, `name` and `age` attributes, the `Student` class also should have `klass` attributes.
+//        There is also an `introduce` method,
+//        The `introduce` method returns a string like:
+//
+//        >My name is Tom. I am 21 years old. I am a Student. I am at Class 2.
+//
+//        The `klass` attribute is an `Object` which has `number` and `leader` attribute.
+//        But the `leader` attribute is `not` in the constructor.
+public class Student extends Person {
+    private Klass klass;
+    public Student(int id, String name, int age, Klass klass){
+        super(id,name,age);
+        this.klass = klass;
 
-public class Student {
+    }
+    public Klass getKlass() {
+        return this.klass;
+    }
+    public String introduce(){
+        if (this.equals(klass.getLeader())){
+            return String.format("My name is %s. I am %d years old. I am a Student. I am Leader of Class %d.",getName(),getAge(),klass.getNumber());
+        }else {
+            return String.format("My name is %s. I am %d years old. I am a Student. I am at Class %d.",getName(),getAge(),klass.getNumber());
+        }
+    }
 }
